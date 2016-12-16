02218174f6cc1c80857da227e3934b9f888b5c55
ssysm <ssysm@users.noreply.github.com>
2016-12-15 21:39:24 -0500

    Update Installer.py
    Add component that for older Ubuntu system.
    添加了给老版本的Ubuntu需要的运行环境。

--- installer.py
+++ installer.py
@@ -35,6 +35,9 @@ def linux_build():
     print O + "[*] Updating..." + W
     sleep(1.5)
     os.system("sudo apt-get update")
+    os.system("sudo apt-get install build-essential autoconf libtool pkg-config python-opengl python-imaging python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev")
+    os.system("sudo easy_install greenlet")
+    os.system("sudo easy_install gevent")
     print O + "[*] Installing essential packages... [*]" + W
     sleep(1.5)
     os.system("sudo apt-get install python-pip python-setuptools python-selenium firefox-esr firefoxdriver")
